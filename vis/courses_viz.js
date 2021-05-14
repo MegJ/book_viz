24bf91f0603844099e08f0ea4ceaf3d4e25ce712
megj <meg.j.rock@gmail.com>
2021-05-14 01:39:17 -0400

    functional for filtering by gender and class year

--- vis/courses_viz.js
+++ vis/courses_viz.js
@@ -2,7 +2,7 @@ function  drawCoursesChart(svgClass, books) {
   console.log("hi")
   let height = 400;
   let width = 800;
-  let margin = ({top: 0, right: 40, bottom: 34, left: 40});
+  let margin = ({top: 0, right: 120, bottom: 34, left: 40});
   
   console.log(books);
   // Data structure describing chart scales
@@ -32,16 +32,16 @@ function  drawCoursesChart(svgClass, books) {
   
   // Colors used for circles depending on continent
   let colors = d3.scaleOrdinal()
-      .domain(["male", "female"])
-      .range(['#D81B60','#1976D2','#388E3C','#FBC02D','#E64A19','#455A64']);
+      .domain(["1000", "2000", "3000", "4000"])
+      .range(['#D81B60','#1976D2','#388E3C','#FBC02D']);
   
-//   d3.select("#asiaColor").style("color", colors("asia"));
-//   d3.select("#africaColor").style("color", colors("africa"));
-//   d3.select("#northAmericaColor").style("color", colors("northAmerica"));
-//   d3.select("#southAmericaColor").style("color", colors("southAmerica"));
-//   d3.select("#europeColor").style("color", colors("europe"));
-//   d3.select("#oceaniaColor").style("color", colors("oceania"));
-  
+
+
+//   d3.select("#color1000").style("color", colors("asia"));
+//   d3.select("#color2000").style("color", colors("\\a"));
+//   d3.select("#color3000").style("color", colors("northAmerica"));
+//   d3.select("#color4000").style("color", colors("southAmerica"));
+
   let svg = d3.select(svgClass)
       .append("svg")
       .attr("width", width)
@@ -58,22 +58,71 @@ function  drawCoursesChart(svgClass, books) {
   let xLine = svg.append("line")
       .attr("stroke", "rgb(96,125,139)")
       .attr("stroke-dasharray", "1,2");
+
+
+  //create display legend
+  let genders = ["male", "female"]
+
+  
+  let legend = genders;
+  d3.select(svgClass).append("g")
+    .selectAll(".key")
+    .data(legend)
+    .enter()
+    .append("circle")
+    .attr("class", "gender_options")
+    .attr('id', function(d, i) { return "key_" + d;})
+    .attr('cx', function(d, i) { return width - (margin.right/2);})
+    .attr('cy', function(d, i) { return 90+ i*25;})
+    .attr('r', 8)
+    .style('fill', '#f8f8f8')
+    .style('stroke', darkTextColor)
+    .style('stroke-width', 2)
+    .on('click', function(d, i) {
+        handleBarLegendChange(d, ".gender_options");
+        svg.selectAll(".books").remove();
+        if(d == "female"){
+            filter_value = "female"
+            redraw(false)
+        } else {
+            filter_value = "male"
+
+            redraw(false)
+        }
+    });
+
+    let filter_field = "gender";
+    let filter_value = "female"
+
+    handleBarLegendChange("female", ".gender_options");
+
+
+    d3.select(svgClass).append("g")
+    .selectAll('.key_labels')
+    .data(legend)
+        .enter()
+        .append('text')
+        .attr('x', function(d, i) { return width-(margin.right/2) + 15;})
+        .attr('y', function(d, i) { return 92 + i*25;})
+        .text(function(d) {return d;})
+        .style('fill', darkTextColor)
+        .style("font-weight", "bold")
+        .style("font-family", "Inconsolata")
+        .style("font-size", "12px");
+
+    // handleBarLegendChange("Women", ".gender_options");
   
   // Create tooltip div and make it invisible
-  let tooltip = d3.select("#svganchor").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 2);
-  
+  let tooltip = addTooltipToVis("heightSvg_tooltip");
+
   // Load and process data
-  d3.csv("https://martinheinz.github.io/charts/data/who_suicide_stats.csv").then(function (data) {
   
-      console.log(data)
       let dataSet = books;
 
       console.log(dataSet);
   
       // Set chart domain max value to the highest total value in data set
-      xScale.domain(d3.extent(data, function (d) {
+      xScale.domain(d3.extent(dataSet, function (d) {
           return +d.year;
       }));
 
@@ -105,7 +154,13 @@ function  drawCoursesChart(svgClass, books) {
       // Trigger filter function whenever checkbox is ticked/unticked
       d3.selectAll("input").on("change", filter);
   
-      function redraw() {
+      function redraw(transition=true) {
+          if(transition){
+              duration = 1000;
+          } else {
+              duration = 0;
+          }
+
   
           // Set scale type based on button clicked
           if (chartState.scale === Scales.lin) {
@@ -116,7 +171,6 @@ function  drawCoursesChart(svgClass, books) {
               xScale = d3.scaleLog().range([ margin.left, width - margin.right ]);
           }
           console.log(chartState.measure)
-          console.log("hhhhhh")
   
           xScale.domain(d3.extent(dataSet, function(d) {
               return +d[chartState.measure];
@@ -137,7 +191,7 @@ function  drawCoursesChart(svgClass, books) {
   
           d3.transition(svg).select(".x.axis")
               .transition()
-              .duration(1000)
+              .duration(duration)
               .call(xAxis);
   
           // Create simulation with specified dataset
@@ -148,7 +202,7 @@ function  drawCoursesChart(svgClass, books) {
                   return xScale(+d[chartState.measure]);  // This is the desired position
               }).strength(2))  // Increase velocity
               .force("y", d3.forceY((height / 2) - margin.bottom / 2))  // // Apply positioning force to push nodes towards center along Y axis
-              .force("collide", d3.forceCollide(9)) // Apply collision force with radius of 9 - keeps nodes centers 9 pixels apart
+              .force("collide", d3.forceCollide(function(d){return 9 + (9 * .5 * (d.count - 1))})) // Apply collision force with radius of 9 - keeps nodes centers 9 pixels apart
               .stop();  // Stop simulation from starting automatically
   
           // Manually run simulation
@@ -157,39 +211,47 @@ function  drawCoursesChart(svgClass, books) {
           }
   
           // Create country circles
-          let countriesCircles = svg.selectAll(".gender")
-              .data(dataSet, function(d) { return d.gender });
-  
+          let countriesCircles = svg.selectAll(".books")
+              .data(dataSet, function(d) { return d.title });
+
+          console.log(countriesCircles.exit());
           countriesCircles.exit()
               .transition()
-              .duration(1000)
+              .duration(duration)
               .attr("cx", 0)
               .attr("cy", (height / 2) - margin.bottom / 2)
               .remove();
   
           countriesCircles.enter()
               .append("circle")
-              .attr("class", "countries")
+              .attr("class", "books")
               .attr("cx", 0)
               .attr("cy", (height / 2) - margin.bottom / 2)
-              .attr("r", 6)
-              .attr("fill", function(d){ return colors(d.gender)})
+              .attr("r", function(d) {return 6 * d.count})
+              .attr("fill", function(d){ 
+
+                    if(d[filter_field] != filter_value){
+                        return lightGreyColor;
+                    } else {
+                        let class_level = d.course_number.substring(d.course_number.indexOf(" "));
+                        class_level = class_level.substring(1, 2) + "000";
+                          return colors(class_level)}
+
+                    })
               .merge(countriesCircles)
               .transition()
-              .duration(2000)
+              .duration(duration * 2)
               .attr("cx", function(d) { return d.x; })
               .attr("cy", function(d) { return d.y; });
   
           // Show tooltip when hovering over circle (data for respective country)
-          d3.selectAll(".countries").on("mousemove", function(d) {
-              console.log(d);
-              tooltip.html(`Country: <strong>${d.country}</strong><br>
-                            ${chartState.legend.slice(0, chartState.legend.indexOf(","))}: 
-                            <strong>${d3.format(",")(d[chartState.measure])}</strong>
-                            ${chartState.legend.slice(chartState.legend.lastIndexOf(" "))}`)
-                  .style('top', d3.event.pageY - 12 + 'px')
-                  .style('left', d3.event.pageX + 25 + 'px')
-                  .style("opacity", 0.9);
+          d3.selectAll(".books").on("mousemove", function(d) {
+              let tooltipText = "<i>" + "<b>"+ d.title +"</b>"+ "</i><br />"+
+                                "author:  " + "<b>" + d.author + "</b><br />" +
+                                "course: <b>" + d.course_number + " " +  d.course_title + "<b><br />" +
+                                "year published: <b>" + d.year;
+
+              updateToolTipText(tooltip, tooltipText, -20, 110);
   
               xLine.attr("x1", d3.select(this).attr("cx"))
                   .attr("y1", d3.select(this).attr("cy"))
@@ -198,7 +260,7 @@ function  drawCoursesChart(svgClass, books) {
                   .attr("opacity", 1);
   
           }).on("mouseout", function(_) {
-              tooltip.style("opacity", 0);
+              hideTooltip(tooltip, "testing");
               xLine.attr("opacity", 0);
           });
   
@@ -220,6 +282,8 @@ function  drawCoursesChart(svgClass, books) {
           }
   
           let checkedBoxes = getCheckedBoxes(".continent");
+
+          console.log(checkedBoxes)
   
           let newData = [];
   
@@ -230,18 +294,65 @@ function  drawCoursesChart(svgClass, books) {
           }
   
           for (let i = 0; i < checkedBoxes.length; i++){
-              let newArray = data.filter(function(d) {
-                  return d.continent === checkedBoxes[i];
+              let newArray = books.filter(function(d) {
+                  let class_level = d.course_number.substring(d.course_number.indexOf(" "));
+                  class_level = class_level.substring(1, 2) + "000";
+                  console.log(class_level);
+                  console.log(checkedBoxes[i]);
+                  console.log(class_level == checkedBoxes[i]);
+                  return class_level == checkedBoxes[i];
               });
               Array.prototype.push.apply(newData, newArray);
           }
   
           dataSet = newData;
+          console.log(dataSet);
           redraw();
       }
   
-  }).catch(function (error) {
-      if (error) throw error;
-  });
+
     
 }
+
+function addTooltipToVis(className) {
+    return d3.select("body")
+      .append("div")
+      .attr("class", className)
+      .style("padding", 10)
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .attr("white-space", "pre-line")
+      .style("background-color", "#fbfbfb")
+      .style("border-radius", "5px")
+      .style("border", "1px solid #cdcdcd");
+  }
+  
+  function updateToolTipText(tooltip, tooltipText, topOffset, leftOffset) {
+    tooltip
+      .html(tooltipText)
+      .style("font-family", "Montserrat")
+      .style("font-size", "12px")
+      .style("visibility", "visible")
+      .style("max-width", 150)
+      .style("top", function() { return event.pageY - topOffset + "px"; })
+      .style("left", function() { return event.pageX - leftOffset +"px"; });
+  }
+  
+  function hideTooltip(tooltip, className) {
+    tooltip.style("visibility", "hidden");
+    d3.selectAll(className).remove();
+  }
+
+  function handleBarLegendChange(d, circle_id) {
+    d3.selectAll(circle_id)
+      .transition()
+      .duration(500)
+      .style("fill", "#f8f8f8");
+  
+    d3.select("#key_" + d)
+      .transition()
+      .duration(500)
+      .style("fill", darkTextColor);
+  }
+  
